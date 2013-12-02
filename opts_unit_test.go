75e067bb58624553ddb896b7b8ecbd11d16e75f9
JP <jpellerin@leapfrogonline.com>
2013-12-02 17:15:37 -0500

    WIP

--- opts_unit_test.go
+++ opts_unit_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestValidateIP4(t *testing.T) {
-	if ret, err := ValidateIp4Address(`1.2.3.4`); err != nil || !ret {
+	if ret, err := ValidateIp4Address(`1.2.3.4`); err != nil || ret == "" {
 		t.Fatalf("ValidateIp4Address(`1.2.3.4`) got %s %s", ret, err)
 	}
 
